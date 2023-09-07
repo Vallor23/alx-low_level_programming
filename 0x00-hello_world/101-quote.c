dde494930e1420fda09b47adfee63cdb1b550fb4
Vallor23 <enosnyabinda2017@gmail.com>
2023-09-07 15:25:07 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the message "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, 59);
+return (1);
+}
+

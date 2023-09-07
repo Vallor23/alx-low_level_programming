b8a56f0eba3025f8f66346615a8870639ed05360
Vallor23 <enosnyabinda2017@gmail.com>
2023-09-07 14:34:59 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "with proper grammar, but the outcome is a piece of art,"
+ *              followed by a new line, using the `printf` function. It returns 0.
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
+

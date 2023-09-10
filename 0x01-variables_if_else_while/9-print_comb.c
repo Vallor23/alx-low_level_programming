1224b891e2d217505b693f8c04accb217da9aa40
Vallor23 <enosnyabinda2017@gmail.com>
2023-09-10 02:06:27 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all possible combinations of
+ * single-digit numbers, separated by ", ", in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number = 0;
+
+while (number < 10)
+{
+putchar(number + 48); /** Convert the number to its ASCII
+			* representation and print
+			*/
+{
+putchar(',');   /* Print the comma*/
+putchar(' ');   /* Print the space*/
+}
+number++;
+}
+
+putchar('\n'); /* Print a newline character*/
+
+return (0);
+}
+

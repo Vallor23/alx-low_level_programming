d262cf4de9cecefd9295bde6680bf4b732a47367
Vallor23 <enosnyabinda2017@gmail.com>
2023-09-10 01:00:44 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all lowercase hexadecimal
+ * digits (0-9 and a-f)
+ * followed by a newline character using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 30;
+int n = 61;
+while (i <= 39)
+while (n <= 66)
+
+{
+putchar(n);
+putchar(i);
+}
+
+/* Print a newline character */
+putchar('\n');
+
+return (0);
+}

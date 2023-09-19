d205980056a3e2b67deddad1f786792993cab19b
Vallor23 <enosnyabinda2017@gmail.com>
2023-09-19 00:05:17 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -19,20 +19,3 @@ void swap_int(int *a, int *b)
     *a = *b;
     *b = temp;
 }
-
-int main()
-{
-    int x, y;
-
-    printf("Enter the value of x: ");
-    scanf("%d", &x);
-
-    printf("Enter the value of y: ");
-    scanf("%d", &y);
-
-    printf("Before swapping: x = %d, y = %d\n", x, y);
-
-    swap_int(&x, &y);
-
-    printf("After swapping: x = %d, y
-

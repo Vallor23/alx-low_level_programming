274694f6eba45dbd9f1817e885a95db9d5e02c65
Vallor23 <enosnyabinda2017@gmail.com>
2023-09-19 20:05:23 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,29 +3,28 @@
 
 /**
  * puts2 - Prints every other character of a string, starting with the
- * first character, followed by a new line.
- * @str: The string to be processed.
+ *first one
+ *@str: input
+ *return: print
  */
 void puts2(char *str)
 {
-int i = 0;
-if (str == NULL)
-return;
-
-while (str[i] != '\0')
+int longi = 0;
+int t = 0;
+char *y = str;
+int o;
+while (*y != '\0')
 {
-putchar(str[i]);
-i += 2; /*Skip every other character*/
-if (str[i] == '\0' && i % 2 == 1)
+y++;
+longi++;
+}
+t = longi - 1;
+for (o = 0 ; o <= t ; o++)
 {
-/**
- * If the last character was skipped and it's an odd-indexed
- *  character, break to avoid out-of-bounds
- */
-break;
+if (o % 2 == 0)
+{
+_putchar(str[o]);
 }
 }
-
-putchar('\n');
+_putchar(str[o]);
 }
-

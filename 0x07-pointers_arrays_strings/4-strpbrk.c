b047e360fcf315831ec0dd4c3342760b4a8f7296
Vallor23 <enosnyabinda2017@gmail.com>
2023-09-25 23:03:13 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: Pointer to the string to search.
+ * @accept: Pointer to the string containing the bytes to search for.
+ *
+ * Description: This function searches for the first occurrence in the string 's'
+ * of any of the bytes specified in the string 'accept'. It returns a pointer to
+ * the byte in 's' that matches one of the bytes in 'accept', or NULL if no such
+ * byte is found.
+ *
+ * Return: A pointer to the byte in 's' that matches one of the bytes in 'accept',
+ * or NULL if no match is found.
+ */
+char *_strpbrk(char *s, char *accept) {
+    if (s == NULL || accept == NULL)
+        return NULL;
+
+    while (*s != '\0') {
+        char *temp = accept;
+        while (*temp != '\0') {
+            if (*s == *temp)
+return (s);
+temp++;
+}
+s++;
+}
+
+    return (NULL); /*No matching byte found in 's'.*/
+}
+

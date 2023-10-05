d63747f961abb3c675cc0be0133a87908f9d5426
Vallor23 <enosnyabinda2017@gmail.com>
2023-10-05 06:36:33 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,25 +10,26 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
-    unsigned int total_size;
- unsigned int i;
- unsigned char *byte_ptr = (unsigned char *)ptr;
-    if (nmemb == 0 || size == 0)
- return (NULL);
+void *ptr;
+unsigned int total_size;
+unsigned int i;
+unsigned char *byte_ptr;
+if (nmemb == 0 || size == 0)
+return (NULL);
 
-    total_size = nmemb * size;
+total_size = nmemb * size;
 
-  ptr = malloc(total_size);
+ptr = malloc(total_size);
 
 if (ptr == NULL)
 return (NULL);
 
 /* Initialize the allocated memory to zero*/
+byte_ptr = (unsigned char *)ptr;
 for (i = 0; i < total_size; i++)
-    {
-        byte_ptr[i] = 0;
-    }
+{
+byte_ptr[i] = 0;
+}
 
-    return (ptr);
+return (ptr);
 }

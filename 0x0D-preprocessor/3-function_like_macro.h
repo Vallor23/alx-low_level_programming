57772f1029ade7fc61f5a9bc3c6607559c344296
Vallor23 <enosnyabinda2017@gmail.com>
2023-10-10 06:06:37 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef ABS_MACRO_H
+#define ABS_MACRO_H
+
+/**
+ * ABS - Computes the absolute value of a number.
+ * @x: The number for which the absolute value is calculated.
+ *
+ * Return: The absolute value of x.
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* ABS_MACRO_H */
+
